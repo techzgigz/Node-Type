0749a60c5b91705ac8ef95de1173b678cd0b9dac
Abhishekjohri1998 <abhishek.johri.659@gmail.com>
2021-09-10 20:26:53 +0530

    second commit

--- src/controllers/syllabus/LessonController.ts
+++ src/controllers/syllabus/LessonController.ts
@@ -18,85 +18,85 @@ import {
     Summary,
   } from "@tsed/schema";
   import { AcceptRoles } from "src/decorators/AcceptRoles";
-  import { Fee } from "src/models/fees/Fee";
-  import { FeesService } from "src/services/FeesService";
+  import{Lesson } from "src/models/syllabus/Lesson";
+  import { LessonService } from "src/services/LessonService";
   
   @Controller("/lesson")
   export class LessonController {
-    constructor(private feesService: FeesService) {}
+    constructor(private lessonService: LessonService) {}
   
     @Get("/")
     @Authorize("jwt")
     @AcceptRoles("admin")
-    @Summary("Return all Fees")
-    @Returns(200, Fee)
-    async getAllFees(@Req() request: Req): Promise<Fee[]> {
+    @Summary("Return all Lesson")
+    @Returns(200, Lesson)
+    async getAllLesson(@Req() request: Req): Promise<Lesson[]> {
       let query = {};
       if ((request.user as any).role !== "superadmin") {
         query = { _id: request.permissions?.readIds };
       }
-      return this.feesService.query(query);
+      return this.lessonService.query(query);
     }
   
     @Get("/:id")
     @Authorize("jwt")
     @AcceptRoles("admin")
-    @Summary("Return Fee based on id")
-    @Returns(200, Fee)
-    async getFee(
+    @Summary("Return Lesson based on id")
+    @Returns(200, Lesson)
+    async getLesson(
       @PathParams("id") id: string,
       @Req() request: Req
-    ): Promise<Fee | null> {
+    ): Promise<Lesson | null> {
       if (
         (request.user as any).role !== "superadmin" &&
         !request.permissions?.readIds?.includes(id)
       ) {
         throw new Error("You don't have sufficient permissions");
       }
-      return this.feesService.find(id);
+      return this.lessonService.find(id);
     }
   
     @Post("/")
     @Authorize("jwt")
     @AcceptRoles("admin")
-    @Summary("Create new Fee")
-    @Returns(201, Fee)
-    async createFee(
+    @Summary("Create new Lesson")
+    @Returns(201, Lesson)
+    async createLesson(
       @Req() request: Req,
-      @Description("Fee model")
+      @Description("Lesson model")
       @BodyParams()
       @Groups("creation")
-      data: Fee
-    ): Promise<Fee> {
+      data: Lesson
+    ): Promise<Lesson> {
       if (request.user) {
-        data = { ...data, createdBy: (request.user as any)._id };
+        data = { ...data};
       }
-      return this.feesService.save(data, {
+      return this.lessonService.save(data, {
         role: (request.user as any).role,
         _id: (request.user as any)._id,
         adminId: (request.user as any).adminId,
       });
     }
   
-    @Put("/:id")
-    @Authorize("jwt")
-    @AcceptRoles("admin")
-    @Summary("Update Fee with id")
-    @Status(201, { description: "Updated Fee", type: Fee })
-    update(
-      @PathParams("id") @Required() id: string,
-      @BodyParams() @Required() @Groups('updation') Fee: Fee
-    ): Promise<Fee | null> {
-      return this.feesService.update(id, Fee);
-    }
+    // @Put("/:id")
+    // @Authorize("jwt")
+    // @AcceptRoles("admin")
+    // @Summary("Update Lesson with id")
+    // @Status(201, { description: "Updated Lesson", type: Lesson })
+    // update(
+    //   @PathParams("id") @Required() id: string,
+    //   @BodyParams() @Required() @Groups('updation') Lesson: Lesson
+    // ): Promise<Lesson | null> {
+    //   return this.lessonService.update(id, Lesson);
+    // }
   
     @Delete("/:id")
     @Authorize("jwt")
     @AcceptRoles("admin")
-    @Summary("Remove a Fee")
+    @Summary("Remove a Lesson")
     @Status(204, { description: "No content" })
     async remove(@PathParams("id") id: string): Promise<void> {
-      await this.feesService.remove(id);
+      await this.lessonService.remove(id);
     }
   }
   
--- src/services/LessonService.ts
+++ src/services/LessonService.ts
@@ -1,46 +1,46 @@
 import { Service, Inject } from "@tsed/common";
 import { EventEmitterService } from "@tsed/event-emitter";
 import { MongooseModel } from "@tsed/mongoose";
-import { Fee } from "src/models/fees/Fee";
+import { Lesson } from "src/models/syllabus/Lesson";
 import { objectDefined } from "src/utils";
 import { EntityCreationUser } from "./PermissionsService";
 
 @Service()
 export class LessonService {
-  @Inject(Fee) private fee: MongooseModel<Fee>;
+  @Inject(Lesson) private lesson: MongooseModel<Lesson>;
   @Inject() private eventEmitter: EventEmitterService;
 
-  async find(id: string): Promise<Fee | null> {
-    const Fee = await this.fee.findById(id).exec();
-    return Fee;
+  async find(id: string): Promise<Lesson | null> {
+    const Lesson = await this.lesson.findById(id).exec();
+    return Lesson;
   }
 
-  async save(data: Fee, user: EntityCreationUser): Promise<Fee> {
-    const Fee = new this.fee(data);
-    await Fee.save();
+  async save(data: Lesson, user: EntityCreationUser): Promise<Lesson> {
+    const Lesson = new this.lesson(data);
+    await Lesson.save();
     this.eventEmitter.emit("entity.created", { user, moduleName: "Fee" });
-    return Fee;
+    return Lesson;
   }
 
-  async update(id: string, data: Fee): Promise<Fee | null> {
-    const Fee = await this.fee.findById(id).exec();
-    if (Fee) {
-      Fee.school = data.school;
-      Fee.grade = data.grade;
-      Fee.medium = data.medium;
-      Fee.info = data.info;
-      Fee.status = data.status;
-      await Fee.save();
-    }
-    return Fee;
-  }
+//   async update(id: string, data: Lesson): Promise<Lesson | null> {
+//     const Lesson = await this.fee.findById(id).exec();
+//     if (Fee) {
+//       Fee.school = data.school;
+//       Fee.grade = data.grade;
+//       Fee.medium = data.medium;
+//       Fee.info = data.info;
+//       Fee.status = data.status;
+//       await Fee.save();
+//     }
+//     return Fee;
+//   }
 
-  async query(options = {}): Promise<Fee[]> {
+  async query(options = {}): Promise<Lesson[]> {
     options = objectDefined(options);
-    return this.fee.find(options).exec();
+    return this.lesson.find(options).exec();
   }
 
-  async remove(id: string): Promise<Fee> {
-    return await this.fee.findById(id).remove().exec();
+  async remove(id: string): Promise<Lesson> {
+    return await this.lesson.findById(id).remove().exec();
   }
 }
